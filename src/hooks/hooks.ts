33708776f198207027ad2c95f2e88a5278c51fc2
Dimitrijevs <dimitrijevgleb@gmail.com>
2025-11-16 13:45:05 +0200

    traces

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -8,7 +8,6 @@ let context: BrowserContext;
 export let page: Page;
 
 BeforeAll(async function () {
-
   getEnv();
 
   browser = await invokeBrowser();
@@ -16,12 +15,42 @@ BeforeAll(async function () {
 
 // to run before each scenario with @Auth tag
 // Before("@Auth", async function () {
-Before(async function () {
+Before(async function ({ pickle }) {
   context = await browser.newContext();
+
+  const scenarioName = pickle.name.replace(/\s+/g, "_") + `_${Date.now()}`;
+
+  await context.tracing.start({
+    name: scenarioName,
+    title: pickle.name,
+    sources: true,
+    screenshots: true,
+    snapshots: true,
+  });
+
   page = await context.newPage();
 });
 
-After(async function () {
+After(async function ({ pickle, result }) {
+
+    console.log(pickle, result);
+
+  const scenarioName = pickle.name.replace(/\s+/g, "_") + `_${Date.now()}`;
+
+  if (result?.status === "FAILED") {
+    console.log(`Scenario '${scenarioName}' failed. Saving trace...`);
+
+    console.log(
+      `Trace saved for failed scenario: traces/${scenarioName}_failed_trace.zip`
+    );
+  } else {
+    console.log(`Scenario '${scenarioName}' passed. Tracing stopped.`);
+  }
+
+  await context.tracing.stop({
+    path: `traces/${scenarioName}_trace.zip`,
+  });
+
   await context.close();
 });
 
